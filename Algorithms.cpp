5d278b8f3feb91dde5ee94e8aa2fdce626c87d81
Oriya-Sigawy <oriyas.07@gmail.com>
2024-04-21 21:53:47 +0300

    started to write the graph's algoritms

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,3 +1,34 @@
+// 214984932 Oriyas.07@gmail.com
+
 #include <iostream>
+#include "Graph.hpp"
 #include "Algorithms.hpp"
-using namespace std;
+namespace ariel
+{
+    bool isConnected(Graph g)
+    {
+        for (int i = 0; i < g.getSize(); i++)
+        {
+            for (int j = 0; j < g.getSize(); j++)
+            {
+                if (i != j && g.at(i).at(j) == 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    string shortestPath(Graph g, int start, int end)
+    {
+    }
+    bool isContainsCycle(Graph g)
+    {
+    }
+    string isBipartite(Graph g)
+    {
+    }
+    string negativeCycle(Graph g)
+    {
+    }
+}
---
+++ Algorithms.hpp
@@ -0,0 +1,14 @@
+//214984932 Oriyas.07@gmail.com
+
+#include <string>
+
+using std::string;
+
+namespace ariel
+{
+    bool isConnected(Graph);
+    string shortestPath(Graph, int, int);
+    bool isContainsCycle(Graph);
+    string isBipartite(Graph);
+    string negativeCycle(Graph);
+}
